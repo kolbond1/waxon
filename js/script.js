91188b4168da69e5212f51e5e69d7bbfb0352885
kolbond1 <78234643+kolbond1@users.noreply.github.com>
2021-02-17 00:03:19 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -37,3 +37,18 @@ $(window).scroll(function(){
       $('.header').removeClass('_scroll')
    }
 });
+
+
+$(document).ready(function(){
+   $('.tabs-triggers__item').click(function(e){
+      e.preventDefault();
+
+      $('.tabs-triggers__item').removeClass('active');
+      $('.tabs-content__item').removeClass('active');
+
+      $(this).addClass('active');
+      $($(this).attr('href')).addClass('active');
+   });
+
+   $('.tabs-triggers__item:first').click();
+});
